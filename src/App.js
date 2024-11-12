bccca66e8fdd64eafd0e94b584a8c85e84cf487c
Louis Kumi <100770360+lkumi17@users.noreply.github.com>
2024-11-12 10:18:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import WorkerRegistrationApp from './components/WorkerRegistrationApp';
 import CertificationCheckApp from './components/CertificationCheckApp';
+import ConnectWallet from './components/ConnectWallet';
 
 const App = () => {
     return (
         <Router>
             <div>
+                <ConnectWallet /> {/* Add the wallet connection component here */}
                 <Routes>
                     <Route path="/register" element={<WorkerRegistrationApp />} />
                     <Route path="/check" element={<CertificationCheckApp />} />
-                    {/* Optional: Redirect the root path to the /register page */}
                     <Route path="/" element={<WorkerRegistrationApp />} />
                 </Routes>
             </div>
